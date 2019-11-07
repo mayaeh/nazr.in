57c7cc0179fbbce4ea7e283f454fe0dca160f8df
Maya Minatsuki <mayaeh@marimo-net.org>
2019-11-07 15:01:07 +0900

    Change to save access log to file. Set the time zone to be output to the access log.

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -4,10 +4,12 @@ import corser from 'corser'
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import mongoose from 'mongoose'
+import moment from 'moment-timezone'
+import path from 'path'
+import rfs from 'rotating-file-stream'
 
 import APIRouter from './routes/api'
 import ShortLink from './models/short-link'
-
 
 // Connect to the MongoDB database
 const databaseURL = process.env.MONGODB_URI || 'mongodb://localhost/nazrin'
@@ -15,12 +17,33 @@ mongoose.connect(databaseURL, { useNewUrlParser: true, useCreateIndex: true, use
   console.error(err)
 })
 
+// set timezone
+const timezone = 'Asia/Tokyo';
+
+morgan.token('date', () => {
+  return moment().tz(timezone).format();
+});
+morgan.format(
+  'combined_custom',
+  ':remote-addr - :remote-user [:date[Asia/Tokyo]] ' +
+  '":method :url HTTP/:http-version" ' +
+  ':status :res[content-length] ":referrer" ":user-agent"',
+);
+
+// create a rotating write stream
+const accessLogStream = rfs('access.log', {
+  size:'10MB',
+  interval: '7d', // rotate weekly
+  compress: 'gzip',
+  path: path.join(__dirname, 'log')
+})
+
 // Create an Express app
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false })) // URL encoded queries
 app.use(bodyParser.json()) // JSON
 app.use(corser.create()) // CORS
-app.use(morgan('combined')) // Logging
+app.use(morgan('combined_custom', {stream: accessLogStream})) // Logging
 
 // API endpoint
 app.use('/api', APIRouter)
