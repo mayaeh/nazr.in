7a2fc07719df8577bd9b5244444265a6ef2f91ff
Delta <46800777+Delta-Time@users.noreply.github.com>
2020-05-28 23:31:38 +0900

    すでにURLが登録されている場合、同じ短縮URLを発行する。 (#150)
    Returns a same short URL for registed URL.

--- packages/api/src/services/url-service.js
+++ packages/api/src/services/url-service.js
@@ -15,13 +15,40 @@ export async function shortenURL(url) {
   const salt = process.env.HASHIDS_SALT || '';
   const minLength = 5;
   const hashids = new Hashids(salt, minLength);
-
   const shortLink = new ShortLink()
-  await shortLink.save()
-  shortLink.url = url
-  shortLink.base62 = hashids.encode(shortLink.numerical_id)
-  await shortLink.save()
+
+  if(await isRegistedURL(url)){
+    const registedLink = await getShortLink(url)
+    shortLink.id = registedLink.numerical_id
+    shortLink.url = registedLink.url
+    shortLink.base62 = registedLink.base62
+  }else {
+    await shortLink.save()
+    shortLink.url = url
+    shortLink.base62 = hashids.encode(shortLink.numerical_id)
+    await shortLink.save()
+  }
   return shortLink
+}
+
+export async function isRegistedURL(url){
+  if (!isURL(url)) {
+    throw new Error('Invalid URL provided')
+  }
+  const registedData = await ShortLink.findOne({ url: url })
+  return registedData != null;
+}
+
+export async function getShortLink(url) {
+  if (!isURL(url)){
+    throw new Error('Invalid URL provided')
+  }
+  const shortLink = await ShortLink.findOne({url: url});
+  return {
+    numerical_id: shortLink.numerical_id, // eslint-disable-line camelcase
+    base62: shortLink.base62,
+    url: shortLink.url,
+  }
 }
 
 export async function getURL(id) {
